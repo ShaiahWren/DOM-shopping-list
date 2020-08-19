6e1219c14eca3c104b8028fa08f509760e745fe4
Shaiah Wren <sde2111@columbia.edu>
2020-08-19 12:05:14 -0400

    lists

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
 'use strict';
+
+
+
+
+const button = document.querySelector('#generateList');
+const myList = document.querySelector('#myList');
+const ul = document.createElement('ul');
+myList.append(ul);
+
+button.addEventListener('click', function (e) {
+    const myListItems = document.querySelectorAll('.input');
+    myListItems.forEach(function(item){
+        const li = document.createElement('li')
+        ul.append(li);
+        li.innerText = item.value;
+        console.log(item.value);
+    })
+
+   
+});
